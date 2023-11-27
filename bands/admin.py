cad36c05b1695e7f17fd3583739efe521945b3ef
agshadow <agshadow@gmail.com>
2023-11-27 16:55:55 +1100

    moved code to bands-admin.py

--- bands/admin.py
+++ bands/admin.py
@@ -1,3 +1,133 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+from bands.models import Musician, Band, Venue, Room
+from datetime import datetime, date
+from django.utils.html import format_html
+from django.urls import reverse
+
+
+class DecadeListFilter(admin.SimpleListFilter):
+    title = "decade born"
+    parameter_name = "decade"
+
+    def lookups(self, request, model_admin):
+        result = []
+
+        this_year = datetime.today().year
+        this_decade = (this_year // 10) * 10
+        start = this_decade - 10
+        for year in range(start, start - 100, -10):
+            result.append((str(year), f"{year}-{year+9}"))
+        return result
+
+    def queryset(self, request, queryset):
+        start = self.value()
+        if start is None:
+            return queryset
+
+        start = int(start)
+        result = queryset.filter(
+            birth__gte=date(start, 1, 1), birth__lte=date(start + 9, 12, 31)
+        )
+        return result
+
+
+@admin.register(Musician)
+class MusicianAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "last_name",
+        "first_name",
+        "birth",
+        "show_weekday",
+        "show_bands",
+    )
+    list_filter = (DecadeListFilter,)
+    search_fields = (
+        "last_name",
+        "first_name",
+    )
+
+    def show_weekday(self, obj):
+        return obj.birth.strftime("%A")
+
+    show_weekday.short_description = "Birth Weekday"
+
+    def show_bands(self, obj):
+        bands = obj.band_set.all()
+        if len(bands) == 0:
+            return format_html("<i>None</i>")
+
+        plural = ""
+        if len(bands) > 1:
+            plural = "s"
+
+        parm = "?id__in=" + ",".join([str(b.id) for b in bands])
+        url = reverse("admin:bands_band_changelist") + parm
+        print(url)
+        return format_html('<a href="{}">Band{}</a>', url, plural)
+
+    show_bands.short_descriptions = "Bands"
+
+
+@admin.register(Band)
+class BandAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    # list_filter = (DecadeListFilter,)
+    search_fields = ("name",)
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ("id", "show_venue_name", "show_rooms")
+    # list_filter = (DecadeListFilter,)
+    search_fields = ("name",)
+
+    def show_rooms(self, obj):
+        rooms = obj.room_set.all()
+        if len(rooms) == 0:
+            return format_html("<i>None</i>")
+        html_rooms = ""
+
+        for r in rooms:
+            print(obj.name)
+            parm = "?id__in=" + str(r.id)
+            url = reverse("admin:bands_room_changelist") + parm
+
+            print(url)
+            html_rooms += "<li><a href='" + url + "'>" + r.name + "</a>"
+            print(html_rooms)
+
+        return format_html(html_rooms)
+
+    show_rooms.short_description = "Rooms"
+
+    def show_venue_name(self, obj):
+        rooms = obj.room_set.all()
+        if len(rooms) == 0:
+            return format_html(obj.name)
+
+        parm = "?id__in=" + ",".join([str(r.id) for r in rooms])
+        url = reverse("admin:bands_room_changelist") + parm
+        print(f"URL: {url}")
+
+        return format_html("<a href = '{}'>" + obj.name + "</a>", url)
+
+    show_venue_name.short_description = "Venue Name"
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    # list_filter = (DecadeListFilter,)
+    search_fields = ("name",)
