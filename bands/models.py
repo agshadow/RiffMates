d57ed73ff434a59e9bf39fadbc754a39264f666d
agshadow <agshadow@gmail.com>
2023-12-08 22:10:05 +1100

    added user_post_save method to add a userprofile using the post_save signal

--- bands/models.py
+++ bands/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Musician(models.Model):
     first_name = models.CharField(max_length=50)
@@ -65,3 +68,19 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     musician_profiles = models.ManyToManyField(Musician, blank=True)
     venues_controlled = models.ManyToManyField(Venue, blank=True)
+
+
+@receiver(post_save, sender=User)
+def user_post_save(sender, **kwargs):
+    # Create UserProfile object if User object is new
+    # and not loaded from fixture
+    print("called post_save")
+    if kwargs["created"] and not kwargs["raw"]:
+        user = kwargs["instance"]
+        try:
+            # Double check UserProfile doesn't exist already
+            # (admin might create it before the signal fires)
+            UserProfile.objects.get(user=user)
+        except UserProfile.DoesNotExist:
+            # No UserProfile exists for this user, create one
+            UserProfile.objects.create(user=user)
