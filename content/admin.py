a0cb093c41d6cf36020015dac8d2b75b8f725037
agshadow <agshadow@gmail.com>
2023-12-14 15:43:20 +1100

    added seekingad midel

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+# Riffmates/content/admin.property
 
-# Register your models here.
+from django.contrib import admin
+from django.utils.text import Truncator
+
+from content.models import SeekingAd
+
+
+@admin.register(SeekingAd)
+class SeekingAdAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "date",
+        "owner",
+        "seeking",
+        "show_ad",
+    )
+
+    def show_ad(self, obj):
+        return Truncator(obj.content).words(5, truncate=" ...")
+        show_ad.short_description = "Ad"
